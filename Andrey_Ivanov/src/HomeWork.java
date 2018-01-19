6b5635171950d76d59730c2a4bb074f1a98c638d
Andrey <34690892+1985IvanovAndrey@users.noreply.github.com>
2018-01-19 18:47:47 +0200

    Update HomeWork.java

--- Andrey_Ivanov/src/HomeWork.java
+++ Andrey_Ivanov/src/HomeWork.java
@@ -1,1 +1,25 @@
+ptivate int countHomeWorks;
+private int countHours;
 
+public getCountHomeWorks(){
+return countHomeWorks;
+}
+
+public setCountHomeWorks(int HomeWorks){
+this.countHomeWorks=HomeWorks;
+}
+public getCountHours(){
+return countHours;
+}
+
+public setCountHours(int countHours){
+this.countcountHours=countHours;
+}
+
+public HomeWork(){
+}
+
+public HomeWork(){
+  setCountHomeWorks();
+  setCountHours();
+}
