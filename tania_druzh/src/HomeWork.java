bdaf2b9cff93e0a2329021b5494659029e24aeb5
Tania <34690858+taniadruzh@users.noreply.github.com>
2018-01-19 18:43:26 +0200

    Create HomeWork.java

---
+++ tania_druzh/src/HomeWork.java
@@ -0,0 +1,26 @@
+publik class HomeWork
+{
+  private int countHours;
+  private int countWorks;
+  
+  public void setCountHours(int countHours)
+  {
+    this.countHours = countHours;
+  }
+  
+  public void setCountWorks(int countWorks)
+  {
+    this.countWorks = countWorks;
+  }
+  
+   public int getCountHours()
+  {
+    return countHours;
+  }
+  
+  public int setCountWorks()
+  {
+    return countWorks;
+  }  
+  
+}
