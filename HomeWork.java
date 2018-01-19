fab0349918cb32d3ecadb5a7b88aaa563eb8d693
Eugene <34690971+EvgenRomanov98@users.noreply.github.com>
2018-01-19 18:53:58 +0200

    Create HomeWork.java

---
+++ HomeWork.java
@@ -0,0 +1,29 @@
+public class HomeWork
+{
+  int DZ;
+  int timeOfDZ;
+  
+    public int getDZ() {
+        return DZ;
+    }
+
+    public void setDZ(int DZ) {
+        this.DZ = DZ;
+    }
+
+    public int getTimeOfDZ() {
+        return timeOfDZ;
+    }
+
+    public void setTimeOfDZ(int timeOfDZ) {
+        this.timeOfDZ = timeOfDZ;
+    }
+    
+     public HomeWork() {
+    }
+
+    public HomeWork(int DZ, int timeOfDZ) {
+        this.DZ = DZ;
+        this.timeOfDZ = timeOfDZ;
+    }
+}
