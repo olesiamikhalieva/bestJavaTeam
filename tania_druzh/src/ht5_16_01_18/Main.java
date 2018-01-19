6919feba385cbbcfb7f7a80f69046b1d3b278803
taniadruzh <tania.druzhchenko@gmail.com>
2018-01-19 20:17:16 +0200

    ht

--- tania_druzh/src/ht5_16_01_18/Main.java
+++ tania_druzh/src/ht5_16_01_18/Main.java
@@ -1,20 +1,16 @@
 package ht5_16_01_18;
 
+import ht4_12_01_18.ClassRoom;
+import ht4_12_01_18.Student;
+
 import java.util.Scanner;
 
-
 public class Main {
-    private static int studCountTotal = 40;
-    private static Student[] studArr= new Student[studCountTotal];
     public static void main(String[] args) {
-        createTotalStudList();
-    }
-
-
-    public static void createTotalStudList()
-    {int countA = 0;
+        Student [] studArr= new Student[40];
+        int countA = 0;
         int countB = 0;
-        for (int i = 0; i < studCountTotal; i++) {
+        for (int i = 0; i < 40; i++) {
             Student stud = new Student();
             studArr[i] = stud;
             if (stud.getAge() <12 ) {
@@ -26,13 +22,13 @@ public class Main {
         }
         int n = 0;
         if (countA < 12) {
-            n = countA;
+           n = countA;
         }
         else {
-            n = 12;
+           n = 12;
         }
         int n1 = 0;
-        Student[] stA = new Student[n];
+        Student [] stA = new Student[n];
         for (int i = 0; i < n; i++){
             for (int j = n1; j < studArr.length; j++){
                 if (studArr[j].getAge() < 12) {
@@ -42,7 +38,7 @@ public class Main {
                 }
             }
         }
-        ClassRoom clA = new ClassRoom("A",12,stA);
+        ht4_12_01_18.ClassRoom clA = new ht4_12_01_18.ClassRoom("A",12,stA);
 
         int k = 0;
         if (countB < 15) {
@@ -51,7 +47,7 @@ public class Main {
         else {
             k = 15;
         }
-        Student[] stB = new Student[k];
+        Student [] stB = new Student[k];
         int k1 = 0;
         for (int i = 0; i < k; i++){
             for (int j = k1; j < studArr.length; j++){
@@ -62,7 +58,8 @@ public class Main {
                 }
             }
         }
-        ClassRoom clB = new ClassRoom("B",15,stB);
+        ht4_12_01_18.ClassRoom clB = new ClassRoom("B",15,stB);
+
         System.out.println("----------ALL----------");
         for (Student student : studArr) {
             System.out.println(student.getName() +" "+ student.getAge());
@@ -97,37 +94,33 @@ public class Main {
             }
         }
         System.out.println("Total not in B: " + countNEnB);
-        printClassTests(clA,clB);
-    }
-
-    public static void printClassTests(ClassRoom clA, ClassRoom clB){
 
         boolean fl = true;
-        while (fl) {
-            System.out.println("Choose method");
-            System.out.println("1. Count of students in classes");
-            System.out.println("2. List of students in classes");
-            System.out.println("3. Short list of students in classes");
-            System.out.println("4. Sort list of students on age asc in classes");
-            System.out.println("5. Sort list of students on age desc in classes");
-            System.out.println("6. Sort list of students on name asc in classes");
-            System.out.println("7. Sort list of students on age desc in classes");
-            System.out.println("8. Find student by name");
-            System.out.println("9. Find class by student's name");
+//        while (fl) {
+//            System.out.println("Choose method");
+//            System.out.println("1. Count of students in classes");
+//            System.out.println("2. List of students in classes");
+//            System.out.println("3. Short list of students in classes");
+//            System.out.println("4. Sort list of students on age asc in classes");
+//            System.out.println("5. Sort list of students on age desc in classes");
+//            System.out.println("6. Sort list of students on name asc in classes");
+//            System.out.println("7. Sort list of students on age desc in classes");
+//            System.out.println("8. Find student by name");
+//            System.out.println("9. Find class by student's name");
 
-            Scanner in = new Scanner(System.in);
-            int ans = in.nextInt();
+//            Scanner in = new Scanner(System.in);
+//            int ans = in.nextInt();
 
-            switch (ans) {
-                case 1: {
+//            switch (ans) {
+//                case 1: {
                     System.out.println("--------------------");
                     System.out.println("----------A----------");
                     System.out.println("Count students in A class is " + clA.getClassCount());
                     System.out.println("----------B----------");
                     System.out.println("Count students in B class is " + clB.getClassCount());
-                    break;
-                }
-                case 2: {
+//                    break;
+//                }
+//                case 2: {
                     System.out.println("--------------------");
                     System.out.println("----------A----------");
                     System.out.println("Students list in A class is: ");
@@ -135,9 +128,9 @@ public class Main {
                     System.out.println("----------B----------");
                     System.out.println("Students list in B class is: ");
                     clB.getClassList();
-                    break;
-                }
-                case 3: {
+//                    break;
+//                }
+//                case 3: {
                     System.out.println("--------------------");
                     System.out.println("----------A----------");
                     System.out.println("Students list in A class is: ");
@@ -145,9 +138,9 @@ public class Main {
                     System.out.println("----------B----------");
                     System.out.println("Students list in B class is: ");
                     clB.getShortClassList();
-                    break;
-                }
-                case 4: {
+//                    break;
+//                }
+//                case 4: {
                     System.out.println("--------------------");
                     System.out.println("----------A----------");
                     System.out.println("Sorted students list in A class is: ");
@@ -157,9 +150,9 @@ public class Main {
                     System.out.println("Sorted students list in B class is: ");
                     clB.ageSort();
                     clB.getClassList();
-                    break;
-                }
-                case 5: {
+//                    break;
+//                }
+//                case 5: {
                     System.out.println("--------------------");
                     System.out.println("----------A----------");
                     System.out.println("Sorted rev students list in A class is: ");
@@ -169,10 +162,10 @@ public class Main {
                     System.out.println("Sorted rev students list in B class is: ");
                     clB.ageRevSort();
                     clB.getClassList();
-                    break;
-                }
-
-                case 6: {
+//                    break;
+//                }
+//
+//                case 6: {
                     System.out.println("--------------------");
                     System.out.println("----------A----------");
                     System.out.println("Sorted name students list in A class is: ");
@@ -182,10 +175,10 @@ public class Main {
                     System.out.println("Sorted name students list in B class is: ");
                     clB.nameSort();
                     clB.getClassList();
-                    break;
-                }
-
-                case 7: {
+//                    break;
+//                }
+//
+//                case 7: {
                     System.out.println("--------------------");
                     System.out.println("----------A----------");
                     System.out.println("Sorted rev name students list in A class is: ");
@@ -195,9 +188,9 @@ public class Main {
                     System.out.println("Sorted rev name students list in B class is: ");
                     clB.nameRevSort();
                     clB.getClassList();
-                    break;
-                }
-                case 8:{
+//                    break;
+//                }
+//                case 8:{
                     System.out.println("Enter student's name:");
                     Scanner inN = new Scanner(System.in);
                     String fName = inN.nextLine();
@@ -207,27 +200,27 @@ public class Main {
                     System.out.println("----------B----------");
                     System.out.println("Students in B class with name  " + fName + ":");
                     clB.findStudent(fName);
-                    break;
-                }
-
-                case 9:{
-                    System.out.println("Enter student's name:");
-                    Scanner inN = new Scanner(System.in);
-                    String fName = inN.nextLine();
+//                    break;
+//                }
+//
+//                case 9:{
+//                    System.out.println("Enter student's name:");
+//                    Scanner inN = new Scanner(System.in);
+                    String sName = "Vasia";
                     System.out.println("----------A----------");
-                    System.out.println("Students with name " + fName +" in class " +  clA.findClass(fName));
+                    System.out.println("Students with name " + sName +" in class " +  clA.findClass(fName));
                     System.out.println("----------B----------");
-                    System.out.println("Students with name " + fName + " in class  " +  clB.findClass(fName));
-                    break;
-                }
-
-                default:{
-                    fl = false;
-                    System.out.println("GoodBye!!");
-                    break;
-                }
-            }
+                    System.out.println("Students with name " + sName + " in class  " +  clB.findClass(fName));
+//                    break;
+//                }
+//
+//                default:{
+//                    fl = false;
+//                    System.out.println("GoodBye!!");
+//                    break;
+                //}
+            //}
         }
 
     }
-}
+//}
