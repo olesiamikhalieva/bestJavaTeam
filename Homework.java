7b76a11165d0541a3483570a87ad55f12259639e
olesiamikhalieva <34690786+olesiamikhalieva@users.noreply.github.com>
2018-01-19 18:42:55 +0200

    Update Homework.java

--- Homework.java
+++ Homework.java
@@ -1,1 +1,16 @@
+int quolityHomework;
+int hoursHomework;
 
+public getquolityHomewok(int qoulityHomework){
+return quolityHomewok;}
+
+public void setquolityHomewok(){
+this.quolityHomework=quolityHomework;
+}
+
+public hoursHomework(int hoursHomework){
+return quolityHomewok;}
+
+public void sethoursHomework(){
+this.hoursHomework=hoursHomework;
+}
