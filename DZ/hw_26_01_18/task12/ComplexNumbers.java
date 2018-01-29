e2258b99b340dfd343c6011d57a08620093da7f2
EvgenRomanov98 <evrom98@gmail.com>
2018-01-29 22:45:17 +0200

    DZ 26.01.18

--- DZ/hw_26_01_18/task12/ComplexNumbers.java
+++ DZ/hw_26_01_18/task12/ComplexNumbers.java
@@ -2,4 +2,32 @@ package task12;
 
 public class ComplexNumbers {
 
+    private double realD;
+    private double imagineD;
+
+    public ComplexNumbers() {
+    }
+
+    public ComplexNumbers(double realD, double imagineD) {
+        this.realD = realD;
+        this.imagineD = imagineD;
+    }
+
+
+    public double getRealD() {
+        return realD;
+    }
+
+    public void setRealD(double realD) {
+        this.realD = realD;
+    }
+
+    public double getImagineD() {
+        return imagineD;
+    }
+
+    public void setImagineD(double imagineD) {
+        this.imagineD = imagineD;
+    }
+
 }
