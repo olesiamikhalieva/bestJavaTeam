265d6bd41c8d7c89f4d2058105d3d3a3cf4848ab
EvgenRomanov98 <evrom98@gmail.com>
2018-02-05 22:09:56 +0200

    task3 DZ 02.02.18

---
+++ DZ/hw_02_02_18/task3/Cat.java
@@ -0,0 +1,9 @@
+package task3;
+
+public class Cat {
+    final private String name = "Васька";
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ DZ/hw_02_02_18/task3/CatRun.java
@@ -0,0 +1,10 @@
+package task3;
+
+public class CatRun {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+
+        System.out.println(cat.getName());
+    }
+
+}
