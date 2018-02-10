88004fdd8d4e0a7718bb04b358266ae42d34eb69
EvgenRomanov98 <evrom98@gmail.com>
2018-02-10 02:09:29 +0200

    DZ 09.02.18

---
+++ DZ/hw_09_02_18/Classes/Coffee/Americano.java
@@ -0,0 +1,23 @@
+package hw_09_02_18.Classes.Coffee;
+
+
+import hw_09_02_18.Classes.Ingrelients.CoffeeAndWater;
+import hw_09_02_18.Classes.Ingrelients.Water;
+
+public class Americano {
+    private CoffeeAndWater coffeeAndWater;
+    private Water water1;
+    private Water water2;
+    public int priceAmericano;
+
+    public Americano(CoffeeAndWater coffeeAndWater, Water water1, Water water2) {
+        this.coffeeAndWater = coffeeAndWater;
+        this.water1 = water1;
+        this.water2 = water2;
+    }
+
+    public int getPriceAmericano() {
+        this.priceAmericano = coffeeAndWater.getPrice() + water2.getPrice() + water1.getPrice();
+        return priceAmericano;
+    }
+}
