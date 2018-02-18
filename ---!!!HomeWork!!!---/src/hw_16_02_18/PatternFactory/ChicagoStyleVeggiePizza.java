3f8418d2d3edeb180998a42d01c303341b0e9075
EvgenRomanov98 <evrom98@gmail.com>
2018-02-18 14:40:18 +0200

    DZ 09.02.18

---
+++ ---!!!HomeWork!!!---/src/hw_16_02_18/PatternFactory/ChicagoStyleVeggiePizza.java
@@ -0,0 +1,12 @@
+package hw_16_02_18.PatternFactory;
+
+public class ChicagoStyleVeggiePizza extends Pizza {
+    public ChicagoStyleVeggiePizza() {
+
+        name = "Chicago Style Veggie";
+        dough = "Chicago thin crust dough Veggie";
+        sauce = "Chicago Veggie sauce";
+
+        toppings.add("Grated reggiano cheese Veggie");
+    }
+}
