39778746051ec546638755fc42ad9c1d41545066
EvgenRomanov98 <evrom98@gmail.com>
2018-03-16 16:53:40 +0200

    DZ 14.03.18

--- ---!!!HomeWork!!!---/src/hw_14_03_18/Task4.java
+++ ---!!!HomeWork!!!---/src/hw_14_03_18/Task4.java
@@ -6,12 +6,13 @@ import java.util.Random;
 public class Task4 {
     public static void main(String[] args) {
         ArrayList<Integer> integers = new ArrayList<>();
-        for (int i = 0; i <120 ; i++) {
+        for (int i = 0; i <50 ; i++) {
             Random random = new Random();
             int num = 25 + random.nextInt(75);
             integers.add(num);
         }
-
-
+        System.out.println(integers);
+        integers.subList(14,42).clear();
+        System.out.println(integers);
     }
 }
