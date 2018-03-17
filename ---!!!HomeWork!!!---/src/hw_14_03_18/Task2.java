d5deac2a8fb418ee6348906569dfde2bfb8fbf2e
EvgenRomanov98 <evrom98@gmail.com>
2018-03-17 12:41:49 +0200

    DZ 14.03.18

--- ---!!!HomeWork!!!---/src/hw_14_03_18/Task2.java
+++ ---!!!HomeWork!!!---/src/hw_14_03_18/Task2.java
@@ -1,25 +1,18 @@
 package hw_14_03_18;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class Task2 {
     public static void main(String[] args) {
 
-        int max = -25;
-        int min = 50;
         ArrayList<Integer> integers = new ArrayList<>();
         Random random = new Random();
 
         for (int i = 0; i < 100; i++) {
             int num = -25 + random.nextInt(50 + 25 + 1);
             integers.add(num);
-            if (num < min) {
-                min = num;
-            }
-            if (num > max) {
-                max = num;
-            }
         }
 
         ArrayList<Integer> integers1 = new ArrayList<>();
@@ -29,8 +22,8 @@ public class Task2 {
         integers.add(22);
         integers.add(5);
 
-        System.out.println("max: " + max);
-        System.out.println("min: " + min);
+        System.out.println("max: " + Collections.min(integers));
+        System.out.println("min: " + Collections.max(integers));
 
         System.out.println(integers.containsAll(integers1));
 
