d9908ee64d8e35550fe0a27e32af4fed0965e46d
EvgenRomanov98 <evrom98@gmail.com>
2018-03-19 20:17:52 +0200

    DZ 16.03.18

--- ---!!!HomeWork!!!---/src/hw_16_03_18/Dictionary.java
+++ ---!!!HomeWork!!!---/src/hw_16_03_18/Dictionary.java
@@ -1,30 +1,28 @@
 package hw_16_03_18;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.concurrent.CountDownLatch;
 
 public class Dictionary {
-    private HashSet<Word> wordHashSet = new HashSet<>();
+    private ArrayList<Word> wordArrayList = new ArrayList<>();
 
     public Dictionary() {
     }
 
-    public Dictionary(HashSet<Word> wordHashSet) {
-        this.wordHashSet = wordHashSet;
+    public Dictionary(ArrayList<Word> words) {
+        this.wordArrayList = words;
     }
 
     public void setWord(Word word) {
-        this.wordHashSet.add(word);
+        this.wordArrayList.add(word);
     }
 
     public void setCollectionWord(Collection wordList) {
-        this.wordHashSet.addAll(wordList);
+        this.wordArrayList.addAll(wordList);
     }
 
     public Word getWord(String word) {
-        for (Word w : wordHashSet) {
+        for (Word w : wordArrayList) {
             if (w.getWord().equals(word)) {
                 return w;
             } else {
@@ -38,27 +36,35 @@ public class Dictionary {
         Word word1 = null;
         Word word2 = null;
         Word word3 = null;
+        int countAll = 0;
         int counter = 1;
-        for (Word w : wordHashSet) {
+        for (Word w : wordArrayList) {
             switch (counter) {
                 case 1:
                     word1 = w;
                     counter++;
+                    countAll++;
                     break;
                 case 2:
                     word2 = w;
                     counter++;
+                    countAll++;
                     break;
                 case 3:
                     word3 = w;
                     counter++;
+                    countAll++;
                     break;
                 default:
                     System.out.println("бубух");
                     break;
             }
-            if (counter == 4)
-                System.out.printf(word1 + " " + word2 + " " + word3);
+            if (counter == 4) {
+                System.out.printf("%-15s%-15s%-15s%n", word1.getWord(), word2.getWord(), word3.getWord());
+                System.out.printf("%-15s%-15s%-15s%n", word1.getTranscription(), word2.getTranscription(), word3.getTranscription());
+                System.out.printf("%-15s%-15s%-15s%n", word1.getTranslate(), word2.getTranslate(), word3.getTranslate());
+                counter = 1;
+            }
         }
     }
 
