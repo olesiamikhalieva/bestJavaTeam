262e9086d857421d6f2b33cc5bac1033796db329
1985IvanovAndrey <dron_krotova@i.ua>
2018-03-19 21:33:01 +0200

    add hw_16_03_18_ Dictionary update

--- Andrey_Ivanov/src/hw_16_03_18/task1_dictionary/Runner.java
+++ Andrey_Ivanov/src/hw_16_03_18/task1_dictionary/Runner.java
@@ -8,37 +8,37 @@ import java.util.Scanner;
 
 public class Runner {
 
-    public static Dictionary[] dict = new Dictionary[20];
+    public static ArrayList<Dictionary> dict = new ArrayList<>();
 
     public static void main(String[] args) {
         setDict();
-       // listFor_9();//вывод 9 слов
-        listFor_12();//вывод 12 слов
+        listFor_9();//вывод 9 слов
+       // listFor_12();//вывод 12 слов
 
 
     }
 
     public static void setDict() {
-        dict[0] = new Dictionary("one", "один", "[ван]");
-        dict[1] = new Dictionary("two", "два", "[ту]");
-        dict[2] = new Dictionary("three", "три", "[сры]");
-        dict[3] = new Dictionary("four", "четыре", "[фо]");
-        dict[4] = new Dictionary("five", "пять", "[файв]");
-        dict[5] = new Dictionary("six", "шесть", "[сыкс]");
-        dict[6] = new Dictionary("seven", "семь", "[сэвэн]");
-        dict[7] = new Dictionary("eight", "восемь", "[ейт]");
-        dict[8] = new Dictionary("nine", "девять", "[найн]");
-        dict[9] = new Dictionary("ten", "десять", "[тэн]");
-        dict[10] = new Dictionary("eleven", "одинадцать", "[илевен]");
-        dict[11] = new Dictionary("twelve", "двенадцать", "[твелв]");
-        dict[12] = new Dictionary("thirteen", "тринадцать", "[сортин]");
-        dict[13] = new Dictionary("fourteen", "четырнадцать", "[фортин]");
-        dict[14] = new Dictionary("fifteen", "пятнадцать", "[фифтин]");
-        dict[15] = new Dictionary("sixteen", "шестнадцать", "[сикстин]");
-        dict[16] = new Dictionary("seventeen", "семнадцать", "[сэвэнтин]");
-        dict[17] = new Dictionary("eighteen", "восемнадцать", "[эйтин]");
-        dict[18] = new Dictionary("nineteen", "девятнадцать", "[найнтин]");
-        dict[19] = new Dictionary("twenty", "двадцать", "[твэнти]");
+        dict.add(new Dictionary("one", "один", "[ван]"));
+        dict.add(new Dictionary("two", "два", "[ту]"));
+        dict.add(new Dictionary("three", "три", "[сры]"));
+        dict.add(new Dictionary("four", "четыре", "[фо]"));
+        dict.add(new Dictionary("five", "пять", "[файв]"));
+        dict.add(new Dictionary("six", "шесть", "[сыкс]"));
+        dict.add(new Dictionary("seven", "семь", "[сэвэн]"));
+        dict.add(new Dictionary("eight", "восемь", "[ейт]"));
+        dict.add(new Dictionary("nine", "девять", "[найн]"));
+        dict.add(new Dictionary("ten", "десять", "[тэн]"));
+        dict.add(new Dictionary("eleven", "одинадцать", "[илевен]"));
+        dict.add(new Dictionary("twelve", "двенадцать", "[твелв]"));
+        dict.add(new Dictionary("thirteen", "тринадцать", "[сортин]"));
+        dict.add(new Dictionary("fourteen", "четырнадцать", "[фортин]"));
+        dict.add(new Dictionary("fifteen", "пятнадцать", "[фифтин]"));
+        dict.add(new Dictionary("sixteen", "шестнадцать", "[сикстин]"));
+        dict.add(new Dictionary("seventeen", "семнадцать", "[сэвэнтин]"));
+        dict.add(new Dictionary("eighteen", "восемнадцать", "[эйтин]"));
+        dict.add(new Dictionary("nineteen", "девятнадцать", "[найнтин]"));
+        dict.add(new Dictionary("twenty", "двадцать", "[твэнти]"));
     }
 
 
@@ -53,22 +53,22 @@ public class Runner {
         switch (n) {
             case 1:
                 for (int i = 0; i < 3; i++) {
-                    int st1 = (int) (Math.random() * dict.length);
-                    int st2 = (int) (Math.random() * dict.length);
-                    int st3 = (int) (Math.random() * dict.length);
-                    System.out.println(dict[st1].getWord() + "\t" + "\t" + dict[st2].getWord() + "\t" + "\t" + dict[st3].getWord());
-                    System.out.println(dict[st1].getTranscription() + "\t" + "\t" + dict[st2].getTranscription() + "\t" + "\t" + dict[st3].getTranscription());
-                    System.out.println(dict[st1].getTranslation() + "\t" + "\t" + dict[st2].getTranslation() + "\t" + "\t" + dict[st3].getTranslation());
+                    int st1 = (int) (Math.random() * dict.size());
+                    int st2 = (int) (Math.random() * dict.size());
+                    int st3 = (int) (Math.random() * dict.size());
+                    System.out.println(dict.get(st1).getWord() + "\t" + "\t" + dict.get(st2).getWord() + "\t" + "\t" + dict.get(st3).getWord());
+                    System.out.println(dict.get(st1).getTranscription() + "\t" + "\t" + dict.get(st2).getTranscription() + "\t" + "\t" + dict.get(st3).getTranscription());
+                    System.out.println(dict.get(st1).getTranslation() + "\t" + "\t" + dict.get(st2).getTranslation() + "\t" + "\t" + dict.get(st3).getTranslation());
                     System.out.println();
                 }
                 break;
             case 2:
                 for (int i = 0; i < 3; i++) {
-                    int st1 = (int) (Math.random() * dict.length);
-                    int st2 = (int) (Math.random() * dict.length);
-                    int st3 = (int) (Math.random() * dict.length);
-                    System.out.println(dict[st1].getWord() + "\t" + "\t" + dict[st2].getWord() + "\t" + "\t" + dict[st3].getWord());
-                    System.out.println(dict[st1].getTranslation() + "\t" + "\t" + dict[st2].getTranslation() + "\t" + "\t" + dict[st3].getTranslation());
+                    int st1 = (int) (Math.random() * dict.size());
+                    int st2 = (int) (Math.random() * dict.size());
+                    int st3 = (int) (Math.random() * dict.size());
+                    System.out.println(dict.get(st1).getWord() + "\t" + "\t" + dict.get(st2).getWord() + "\t" + "\t" + dict.get(st3).getWord());
+                    System.out.println(dict.get(st1).getTranslation() + "\t" + "\t" + dict.get(st2).getTranslation() + "\t" + "\t" + dict.get(st3).getTranslation());
                     System.out.println();
                 }
                 break;
@@ -86,25 +86,25 @@ public class Runner {
         switch (n) {
             case 1:
                 for (int i = 0; i < 3; i++) {
-                    int st1 = (int) (Math.random() * dict.length);
-                    int st2 = (int) (Math.random() * dict.length);
-                    int st3 = (int) (Math.random() * dict.length);
-                    int st4 = (int) (Math.random() * dict.length);
-                    System.out.println(dict[st1].getWord() + "\t" + "\t" + dict[st2].getWord() + "\t" + "\t" + dict[st3].getWord()+"\t"+"\t"+dict[st4].getWord());
-                    System.out.println(dict[st1].getTranscription() + "\t" + "\t" + dict[st2].getTranscription() + "\t" + "\t" + dict[st3].getTranscription()+"\t"+"\t"+dict[st4].getTranscription());
-                    System.out.println(dict[st1].getTranslation() + "\t" + "\t" + dict[st2].getTranslation() + "\t" + "\t" + dict[st3].getTranslation()+"\t"+"\t"+dict[st4].getTranslation());
+                    int st1 = (int) (Math.random() * dict.size());
+                    int st2 = (int) (Math.random() * dict.size());
+                    int st3 = (int) (Math.random() * dict.size());
+                    int st4 = (int) (Math.random() * dict.size());
+                    System.out.println(dict.get(st1).getWord() + "\t" + "\t" + dict.get(st2).getWord() + "\t" + "\t" + dict.get(st3).getWord()+"\t"+"\t"+dict.get(st4).getWord());
+                    System.out.println(dict.get(st1).getTranscription() + "\t" + "\t" + dict.get(st2).getTranscription() + "\t" + "\t" + dict.get(st3).getTranscription()+"\t"+"\t"+dict.get(st4).getTranscription());
+                    System.out.println(dict.get(st1).getTranslation() + "\t" + "\t" + dict.get(st2).getTranslation() + "\t" + "\t" + dict.get(st3).getTranslation()+"\t"+"\t"+dict.get(st4).getTranslation());
                     System.out.println();
                 }
                 break;
             case 2:
                 for (int i = 0; i < 3; i++) {
-                    int st1 = (int) (Math.random() * dict.length);
-                    int st2 = (int) (Math.random() * dict.length);
-                    int st3 = (int) (Math.random() * dict.length);
-                    int st4 = (int) (Math.random() * dict.length);
+                    int st1 = (int) (Math.random() * dict.size());
+                    int st2 = (int) (Math.random() * dict.size());
+                    int st3 = (int) (Math.random() * dict.size());
+                    int st4 = (int) (Math.random() * dict.size());
 
-                    System.out.println(dict[st1].getWord() + "\t" + "\t" + dict[st2].getWord() + "\t" + "\t" + dict[st3].getWord()+"\t"+"\t"+dict[st4].getWord());
-                    System.out.println(dict[st1].getTranslation() + "\t" + "\t" + dict[st2].getTranslation() + "\t" + "\t" + dict[st3].getTranslation()+"\t"+"\t"+dict[st4].getTranslation());
+                    System.out.println(dict.get(st1).getWord() + "\t" + "\t" + dict.get(st2).getWord() + "\t" + "\t" + dict.get(st3).getWord()+"\t"+"\t"+dict.get(st4).getWord());
+                    System.out.println(dict.get(st1).getTranslation() + "\t" + "\t" + dict.get(st2).getTranslation() + "\t" + "\t" + dict.get(st3).getTranslation()+"\t"+"\t"+dict.get(st4).getTranslation());
                     System.out.println();
                 }
                 break;
