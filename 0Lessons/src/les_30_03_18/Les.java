7e630eee0cd54482ee63d320fdd9dfaa8ed2665b
poletaiev@gmail.com <slayers99>
2018-03-30 20:57:16 +0300

    les

--- 0Lessons/src/les_30_03_18/Les.java
+++ 0Lessons/src/les_30_03_18/Les.java
@@ -1,14 +1,38 @@
 package les_30_03_18;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
 
 /**
  * Created by java on 30.03.2018.
  */
 public class Les {
     public static void main(String[] args) throws IOException {
-        File file = new File("0Lessons//src//les_30_03_18//Hellow.txt");
-        file.createNewFile();
+
+        InputStream url = new URL("https://www.google.com.ua").openStream();
+
+        System.out.println(url.read());
+        try(InputStream inputStream = new URL("https://www.google.com.ua").openStream()) {
+            int i = -1;
+            int spaceCount = 0;
+            int slashCount = 0;
+            int trCount = 0;
+
+            while ((i = inputStream.read()) != -1) {
+                if ((char) i == ' ') {
+                    spaceCount++;
+                }
+                if ((char) i == '/') {
+                    slashCount++;
+                }
+                if ((char) i == '>') {
+                    trCount++;
+                }
+            }
+            System.out.println("spaces: " + spaceCount);
+            System.out.println("slashes: " + slashCount);
+            System.out.println(">: " + trCount);
+        }
+
     }
 }
