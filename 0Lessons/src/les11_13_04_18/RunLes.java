7ac06d1700e73f95f0d5b097dae67d2c855b4c8a
poletaiev@gmail.com <slayers99>
2018-04-13 21:00:22 +0300

    les

---
+++ 0Lessons/src/les11_13_04_18/RunLes.java
@@ -0,0 +1,14 @@
+package les11_13_04_18;
+
+import les10_10_04_18.ATM;
+
+/**
+ * Created by java on 13.04.2018.
+ */
+public class RunLes {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class testClass = ATM.class;
+        System.out.println(testClass);
+
+    }
+}
