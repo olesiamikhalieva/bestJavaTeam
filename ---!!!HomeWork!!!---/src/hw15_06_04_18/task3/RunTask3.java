a266081144e320a056a46cf0d50880a0e9192d0e
EvgenRomanov98 <evrom98@gmail.com>
2018-04-13 16:44:33 +0300

    DZ 06.04.18

--- ---!!!HomeWork!!!---/src/hw15_06_04_18/task3/RunTask3.java
+++ ---!!!HomeWork!!!---/src/hw15_06_04_18/task3/RunTask3.java
@@ -9,20 +9,22 @@ public class RunTask3 {
         File file = new File("---!!!HomeWork!!!---/src/hw15_06_04_18/task3/file.txt");
         System.out.println(file.createNewFile());
 
-        try (InputStream inputStream = new URL("https://icpc.baylor.edu/login").openStream();
-             BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file.getParent() + "/" + file.getName()))) {
+        InputStream inputStream = new URL("https://icpc.baylor.edu/login").openStream();
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file.getParent() + "/" + file.getName()));
+
+        try {
 
             StringBuilder str = new StringBuilder("");
-            int i = 0;
-            while ((i = bufferedInputStream.read()) != -1) {
-                str.append((char) i);
+            while ((str.append((char) bufferedInputStream.read()) != null)) {
             }
-
             bufferedWriter.write(String.valueOf(str));
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
+        } finally {
+            bufferedInputStream.close();
+            bufferedWriter.close();
         }
     }
 }
