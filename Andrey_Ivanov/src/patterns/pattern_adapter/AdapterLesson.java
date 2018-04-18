7679eadeef14ad3a7cf4d5ab2f179c454ec4f301
1985IvanovAndrey <dron_krotova@i.ua>
2018-04-19 00:30:04 +0300

    add pattern_adapter

---
+++ Andrey_Ivanov/src/patterns/pattern_adapter/AdapterLesson.java
@@ -0,0 +1,50 @@
+package patterns.pattern_adapter;
+
+public class AdapterLesson {
+
+    public static void main(String[] args) {
+        CarWash carWash = new CarWash();
+        carWash.washCar(new TrackWrap(new MyTrack()));
+    }
+}
+
+class TrackWrap implements Car {
+    Track track;
+
+    public TrackWrap(Track track) {
+        this.track = track;
+    }
+
+    @Override
+    public void wash() {
+        track.clean();
+    }
+}
+
+interface Track {
+    void clean();
+}
+
+class MyTrack implements Track {
+    @Override
+    public void clean() {
+        System.out.println("Track is clean");
+    }
+}
+
+interface Car {
+    void wash();
+}
+
+class CarWash {
+    public void washCar(Car car) {
+        car.wash();
+    }
+}
+
+class Audi implements Car {
+    @Override
+    public void wash() {
+        System.out.println("wash car");
+    }
+}
